dfb591faab06e135661ca0b42f6cc0a6f6f014b1
Nishacid <nishacid@protonmail.com>
2022-02-20 17:26:26 +0100

    error update

--- main.py
+++ main.py
@@ -2,62 +2,67 @@
 # -*- coding: utf-8 -*- 
 # Python Version : 3.X
 
+import os
 import mmh3
 import codecs
 import requests
 import argparse
-import warnings
 import shodan
 import yaml
 import sys
 
 # Arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("--api", dest="API", help="Shodan API Key", required=True)
-parser.add_argument("--org", dest="org", help="Targeted organization", required=False)
-parser.add_argument("--url", dest="url", help="URL of the favicon", required=False)
-parser.add_argument("--img", dest="img", help="Image source of the favicon", required=False)
-parser.add_argument("--hash", dest="hash", help="Hash of the favicon", required=False)
-parser.add_argument("--common", dest="common", help="Common Favicon Scan", action="store_true", required=False)
-args = parser.parse_args()
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--api", dest="API", default=None, help="Shodan API Key", required=True)
+    parser.add_argument("-o", "--org", dest="org", default=None, help="Targeted organization", required=False)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-u", "--url", dest="url", default=None, help="URL of the favicon")
+    group.add_argument("-i", "--img", dest="img", default=None, help="Image source of the favicon")
+    group.add_argument("-H", "--hash", dest="hash", default=None, help="Hash of the favicon")
+    group.add_argument("-c", "--common", dest="common", default=None, help="Common Favicon Scan", action="store_true")
+    return parser.parse_args()
 
-# A free API key will not working
-SHODAN_API_KEY = args.API
-api = shodan.Shodan(SHODAN_API_KEY)
+# Calculate the hash of the favicon with an url
+def get_hash_from_url(url, verify=False):
+    if not verify:
+        # Disable warings of insecure connection for invalid certificates
+        requests.packages.urllib3.disable_warnings()
+        # Allow use of deprecated and weak cipher methods
+        requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+        try:
+            requests.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+        except AttributeError:
+            pass
 
-# calculate the hash of the favicon with an url
-def get_hash_from_url(url):
-    
-    # Disable SSL Warning
-    warnings.filterwarnings('ignore', message='Unverified HTTPS request')
-    
     # Calcul Hash
     session = requests.session()
-    favdata = session.get(url, verify=False)
+    favdata = session.get(url, verify=verify)
     if 'Content-Type' in favdata.headers:
         if "text/html" not in favdata.headers['Content-Type']:
             favicon = codecs.encode(favdata.content, "base64")
             favhash = mmh3.hash(favicon)
             return favhash
-    
-# calculate the hash of the favicon with an image
+
+
+# Calculate the hash of the favicon with an image
 def get_hash_from_image(image):
     f = open(image, "rb")
     data = f.read()
     favicon = codecs.encode(data, "base64")
     favhash = mmh3.hash(favicon)
     return favhash
-        
+
+
 # Shodan Scan
-def scan_shodan(argument):
-    try:      
-        if args.org:
-            org = args.org
+def scan_shodan(argument, org=None):
+    try:
+        if org is not None:
             results = api.search(f'http.favicon.hash:{argument} org:{org}')
         else:
             results = api.search(f'http.favicon.hash:{argument}')
         print(f"[+] Total results found : {results['total']}\n")
-        
+
         for result in results['matches']:
             print(f"IP : {result['ip_str']}")
             # print(f"Hostname : {result['hostnames']}")
@@ -65,60 +70,60 @@ def scan_shodan(argument):
             # print(f"OS : {result['os']}")
             print(f"Organization : {result['org']}\n")
     except shodan.APIError as e:
-            print(f"Error : {e}")
-            sys.exit()
+        print(f"Error : {e}")
+        sys.exit()
 
-# Scan of the Yaml Database    
+
+# Scan of the Yaml Database
 def common_scan():
-    with open("database.yml", "r") as database:
-        try:
-            data = yaml.safe_load(database)
-        except yaml.YAMLError as e:
-            print(f"Error : {e}")
-            sys.exit()
-        num = 1
-        for value in data.items():
-            hash = data[num]["hash"]
-            name = data[num]["name"]
-            print(f"[+] Scanning for {name}")
-            scan_shodan(hash)
-            num += 1
+    if os.path.exists("database.yml"):
+        with open("database.yml", "r") as database:
+            try:
+                data = yaml.safe_load(database)
+            except yaml.YAMLError as e:
+                print(f"Error : {e}")
+                sys.exit()
+            num = 1
+            for value in data.items():
+                hash = data[num]["hash"]
+                name = data[num]["name"]
+                print(f"[+] Scanning for {name}")
+                scan_shodan(hash, org=options.org)
+                num += 1
+    else:
+        print("[!] database.yml pas trouvé")
 
-if __name__ == "__main__": 
-    
-    if args.common and not (args.hash or args.url or args.img):
-        print("[+] Perfoming a common scan...")
+if __name__ == '__main__':
+
+    options = parseArgs()
+
+    # A free API key will not working
+    SHODAN_API_KEY = options.API
+    api = shodan.Shodan(SHODAN_API_KEY)
+
+    if options.common is not None:
+        print("[+] Performing a common scan...")
         common_scan()
-        
+
     # URL Scan
-    elif args.url and not (args.hash or args.img or args.common):
-        url = args.url
+    elif options.url is not None:
+        url = options.url
         url_hash = get_hash_from_url(url)
         print(f"[+] The hash of the favicon is : {url_hash}")
-        print("[+] Perfoming a shodan scan...")
-        scan_shodan(url_hash)
-        
+        print("[+] Performing a shodan scan...")
+        scan_shodan(url_hash, org=options.org)
+
     # Hash Scan
-    elif args.hash and not (args.url or args.img or args.common):
-        hash = args.hash
+    elif options.hash is not None:
+        hash = options.hash
         print(f"[+] The hash of the favicon is : {hash}")
-        print("[+] Perfoming a shodan scan...")
-        scan_shodan(hash)
-        
+        print("[+] Performing a shodan scan...")
+        scan_shodan(hash, org=options.org)
+
     # Image Scan
-    elif args.img and not (args.url or args.hash or args.common):
-        image = args.img
+    elif options.img is not None:
+        image = options.img
         img_hash = get_hash_from_image(image)
         print(f"[+] The hash of the favicon is : {img_hash}")
-        print("[+] Perfoming a shodan scan...")
-        scan_shodan(img_hash)
-        
-    # Too many arguments
-    elif (args.img and args.url) or (args.img and args.hash) or (args.url and args.hash):
-        print("[-] Too many arguments, please choose only --url or --img or --hash")
-        sys.exit()
-    
-    # No arguments
-    elif not args.img or not args.url or not args.hash or args.common:
-        print("[-] Please choose one argument")
-        sys.exit()
+        print("[+] Performing a shodan scan...")
+        scan_shodan(img_hash, org=options.org)
